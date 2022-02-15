7e2044b18663d2c15ecaa0b81154073e99d3f004
isabellacpmelo <isacpmelo@gmail.com>
2022-02-15 15:23:54 -0300

    Tipos primitivos e saída de dados

---
+++ 2. Tratando dados e fazendo contas/desafio-003_refeito.py
@@ -0,0 +1,7 @@
+# Aula sobre tipos primitivos e saída de dados.
+# Desafio 003: Crie um programe que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+print('A soma entre {0} e {1} é igual a {2}.'.format(n1, n2, s))
---
+++ 2. Tratando dados e fazendo contas/desafio-004.py
@@ -0,0 +1,12 @@
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ela.
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é', type(a))
+print('Só tem espaços? ', a.isspace())
+print('É numérico? ', a.isnumeric())
+print('É alfabético? ', a.isalpha())
+print('É alfanumérico? ', a.isalnum())
+print('Está em maiúsculas?', a.isupper())
+print('Está em minúsculas?', a.islower())
+print('Está capitalizada?', a.istitle())
+
+
